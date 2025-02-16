df1572d20cb87ed3d2a64825574eb76a1aafe25f
shahal <shahalsmn@gmail.com>
2025-02-16 12:10:38 +0530

    Update cart.proto and regenerate pb files

--- proto/cart_grpc.pb.go
+++ proto/cart_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.29.3
-// source: proto/cartpb/cart.proto
+// source: cartpb/cart.proto
 
 package proto
 
@@ -193,5 +193,5 @@ var CartService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "proto/cartpb/cart.proto",
+	Metadata: "cartpb/cart.proto",
 }
